2c5aa514991fc6bffaad8f5cfb59cfbd32391ac6
Jasmine Huang <tinjasminee@gmail.com>
2020-05-26 15:39:55 +0800

    logic finish

--- config/routes.rb
+++ config/routes.rb
@@ -5,21 +5,21 @@ Rails.application.routes.draw do
 
   # As a user, I can add a new task
   # step1 get the form
-  get "tasks/new", to: "tasks#new", as: :new_task
+  get "tasks/new", to: "tasks#new"
 
   # As a user, I can view the details of a task
   get "tasks/:id", to: "tasks#show", as: :task
 
   # step2 post create form
-  post "tasks", to: "tasks#create"
+  post "tasks", to: "tasks#create", as: :new_task
 
   # As a user, I can edit a task (mark as completed / update title & details)
   # Step 1, GET the form
-  get "tasks/:id/edit", to: "tasks#edit"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
   # Step 2, PATCH the form
   patch "tasks/:id", to: "tasks#update"
 
   # As a user, I can remove a task
-  delete "tasks/:id", to: "tasks#destroy"
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
 
 end
